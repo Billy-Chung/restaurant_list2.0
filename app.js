af361a111774f2b2fa9cfc054cdc9896b7aba606
鄭又誠 <zxcy9980@gmail.com>
2020-05-15 15:55:16 +0800

    feat: todo modles finish

--- app.js
+++ app.js
@@ -2,6 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose') // 載入 mongoose
+// 設定連線到 mongoDB
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true }) 
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+//json的清單
 const restaurantList = require('./restaurant.json')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -10,26 +25,26 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  
-    res.render('index',{ restaurants: restaurantList.results });
-  })
+
+  res.render('index', { restaurants: restaurantList.results });
+})
 
 app.get('/restaurants/:restaurant_id', (req, res) => {
-  
-    const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id) 
-    
-    res.render('show', { restaurant: restaurant })
-  })
 
- 
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+
+  res.render('show', { restaurant: restaurant })
+})
+
+
 app.listen(port, () => {
-    console.log(`Express is listening on localhost:${port}`)
-  })
+  console.log(`Express is listening on localhost:${port}`)
+})
 
-  app.get('/search', (req, res) => {
-    const keyword = req.query.keyword
-    const restaurants = restaurantList.results.filter(restaurant => {
-      return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
-    })
-    res.render('index', { restaurants: restaurants , keyword: keyword})
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
   })
+  res.render('index', { restaurants: restaurants, keyword: keyword })
+})
---
+++ models/todo.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const todoSchema = new Schema({
+    results: {
+        id: Number,
+        name: string,
+        name_en: string,
+        category: string,
+        image: string,
+        location: string,
+        phone: Number,
+        google_map: string,
+        rating: Number,
+        description: string
+    }
+}
+})
+module.exports = mongoose.model('Todo', todoSchema)
